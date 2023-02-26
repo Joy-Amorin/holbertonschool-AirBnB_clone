dc5f79b54a4ad2a78331b945c715e14d5ba0082d
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-26 09:33:15 -0800

    import storage, fix syntax

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,13 @@
 """ Module doc"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """ Class doc"""
     def __init__(self, *args, **kwargs):
-        """ __init__ doc"""
+        """__init__ doc"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -22,17 +23,18 @@ class BaseModel:
 
     def __str__(self):
         """return str"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
         """updates the public instance attribute
         updated_at with the current datetime """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """generate a dictionary representation of an instance"""
         dic = self.__dict__.copy()
         dic.update({"__class__": self.__class__.__name__,
                     "created_at": self.created_at.isoformat(),
-                    "updated_at": self.updated_at.isoformat()})
+                     "updated_at": self.updated_at.isoformat()})
         return dic
