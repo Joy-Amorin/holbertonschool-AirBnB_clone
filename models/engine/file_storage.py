aea0ec9d60c990c26c4326709a95ee999a646600
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-27 08:46:51 -0800

    fix syntax save method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module """
-import os
 import json
+import os.path
 
 
 class FileStorage:
@@ -18,22 +18,23 @@ class FileStorage:
     def new(self, obj):
         """sets obj"""
 
-        FileStorage.__objects[f"{obj.__class__.__name__} {obj.id}"] = obj
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         """serializes"""
-        with open(FileStorage.__file_path, "w") as f:
-            obj_dict = {}
-            for key, value in FileStorage.__objects.items():
-                obj_dict[key] = value.to_dct()
-                json.dump(obj_dict, f)
+        obj_dict = {}
+        for key, value in FileStorage.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(obj_dict, f)
 
-    def reload (self):
+    def reload(self):
         """deserializes the JSON fie"""
+        from models.base_model import BaseModel
         try:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-                dict_j = json.loads(f)
-                for key, value in dict_j.items():
-                    FileStorage.__objects[key] = BaseModel.new(**value)
+                dict_j = json.loads(f.read())
+            for key, value in dict_j.items():
+                FileStorage.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
             pass
