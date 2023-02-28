3ddc7200caa4352ffd7110f7000210129e60d14d
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-28 07:30:37 -0800

    tsk_9 classes

---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""write class"""
+
+class Amenity(BaseModel):
+    """Amenity class"""
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+   """write classes that inherit from BaseModel"""
+
+
+class State(BaseModel):
+    """State class"""
+
+    name = ""
