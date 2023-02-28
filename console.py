b50b022788dde118d9a0d10e28e55120279830cf
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-27 16:09:43 -0800

    console 0.1

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """console"""
 import cmd
 import sys
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +22,64 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """emptyline"""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it
+        (to the JSON file) and prints the id"""
+
+        if not arg:
+            print("""** class name missing **""")
+            return
+        if arg != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        else:
+            inst = BaseModel()
+            storage.save()
+            print(inst.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+
+        argg = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if argg[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        if len(argg) == 1:
+            print("** instance id missing **")
+            return
+
+        strr = str(argg[0]) + "." + str(argg[1])
+        print(strr)
+
+        if strr in storage.all():
+            print(storage.all()[strr])
+        else:
+            print("** no instance found ** ")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+
+        argg = arg.split()
+        if not argg:
+            print("** class name missing **")
+        elif argg[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(argg) == 1:
+            print("** instance id missing **")
+
+        else:
+            strr = str(argg[0]) + "." + argg[1]
+            if strr in storage.all():
+                storage.all().pop(strr)
+                storage.save()
+            else:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
